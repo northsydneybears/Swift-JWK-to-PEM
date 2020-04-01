b220443466d290f07f88ef39c537eed73bb03943
Peter Gow <info@jerrycan.co>
2020-04-01 14:57:22 +1100

    Swap Crypto for OpenCrypto in Vapor 4

--- Package.swift
+++ Package.swift
@@ -13,15 +13,15 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio-ssl-support.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/open-crypto.git", from: "4.0.0-beta.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "CJWKPEMOpenSSL"),
-        .target(
-            name: "SwiftJWKtoPEM",
-            dependencies: ["CJWKPEMOpenSSL", "Crypto"]),
+        .target(name: "SwiftJWKtoPEM", dependencies: [
+            "CJWKPEMOpenSSL",
+            .product(name: "OpenCrypto", package: "open-crypto")]),
         .testTarget(
             name: "SwiftJWKtoPEMTests",
             dependencies: ["SwiftJWKtoPEM"]),
